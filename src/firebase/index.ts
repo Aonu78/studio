6e0a65c4993388021ae41c8a7dfa07148b957869
Syed Aoon Hussain <syedaoonhussain@gmail.com>
2025-09-27 16:57:26 +0000

    remove firestore save on local user session or storage, as user rejoin t

--- src/firebase/index.ts
+++ src/firebase/index.ts
@@ -3,7 +3,7 @@
 import { firebaseConfig } from '@/firebase/config';
 import { initializeApp, getApps, getApp, FirebaseApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
-import { getFirestore } from 'firebase/firestore'
+import { getFirestore, initializeFirestore, memoryLocalCache } from 'firebase/firestore'
 
 // IMPORTANT: DO NOT MODIFY THIS FUNCTION
 export function initializeFirebase() {
@@ -22,10 +22,14 @@ export function initializeFirebase() {
 }
 
 export function getSdks(firebaseApp: FirebaseApp) {
+  const firestore = initializeFirestore(firebaseApp, {
+    localCache: memoryLocalCache(),
+  });
+
   return {
     firebaseApp,
     auth: getAuth(firebaseApp),
-    firestore: getFirestore(firebaseApp)
+    firestore: firestore,
   };
 }
 
