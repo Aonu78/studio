88526754cf75a7282df67796860ebec58fd04fb3
Syed Aoon Hussain <syedaoonhussain@gmail.com>
2025-09-27 11:10:35 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-1406273906-a6516",
+  "appId": "1:890634247347:web:e4d61e980c8dc72d8324c6",
+  "apiKey": "AIzaSyAeCEWNRDjQZYjCGL8pm-C_8iRL4zDuj_g",
+  "authDomain": "studio-1406273906-a6516.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "890634247347"
+};
