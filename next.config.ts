d06a1227cb38087e140f3707c6fccd06d4e9a50c
Syed Aoon Hussain <syedaoonhussain@gmail.com>
2025-09-27 12:24:37 +0000

    update path

--- next.config.ts
+++ next.config.ts
@@ -24,13 +24,20 @@ const nextConfig: NextConfig = {
       },
     ],
   },
-  experimental: {
-    // This is to allow the Next.js dev server to be accessed from the
-    // Firebase Studio preview URL.
-    allowedDevOrigins: [
-        'https://*.cloudworkstations.dev',
-    ],
-  },
+  async headers() {
+    return [
+      {
+        // matching all API routes
+        source: "/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "*" },
+          { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
+          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+        ]
+      }
+    ]
+  }
 };
 
 export default nextConfig;
